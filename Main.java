a56f7d93a3d2d345ae092f81f75c370c2415f81f
Julia Lu Dac <julialudac@yahoo.fr>
2019-05-19 18:27:01 +0200

    Updated files

--- Main.java
+++ Main.java
@@ -6,6 +6,8 @@ class Main{
     public static void main(String[] args)
     {
         Toto.sayToto();
+        Toto t = new Toto();
+        System.out.println(t.getAge());
     }
     
     
--- Toto.java
+++ Toto.java
@@ -3,8 +3,15 @@
 
 class Toto{
     
+    private int age = 20;
+    
     public static void sayToto(){
         System.out.println("Toto!!!");
+    }
+    
+    public int getAge()
+    {
+        return age;
     }
     
     
